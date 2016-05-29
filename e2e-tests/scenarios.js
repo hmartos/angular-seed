9ce425fee5ea4753657b7a3d723c783b5e210565
hmartos <hectormartosg@gmail.com>
2016-05-29 22:52:49 +0200

    Imporcements in end to end tests

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -4,22 +4,43 @@
 
 describe('my app', function() {
 
+    //TODO inject $translate service to check texts
 
     it('should automatically redirect to /home when location is /app', function() {
         browser.get('app');
         expect(browser.getLocationAbsUrl()).toMatch("/home");
     });
 
-
     describe('home', function() {
 
-        beforeEach(function() {
-            browser.get('app/#/home');
+        it('should render home when user navigates to home', function() {
+            expect(element.all(by.id('forked')).first().getText()).toBe("Forked from angular/angular-seed");
         });
 
+        it('should change language between english and spanish', function() {
+            //Get dropdown
+            var languageSwitcher = element(by.css("div[uib-dropdown]"));
 
-        it('should render home when user navigates to home', function() {
+            //Change language to spanish
+            languageSwitcher.element(by.id('languageSwitcher')).click();
+            languageSwitcher.element(by.id('toSpanish')).click();
             expect(element.all(by.id('welcome')).first().getText()).toMatch(/Bienvenido/);
+
+            //Change language to english
+            languageSwitcher.element(by.id('languageSwitcher')).click();
+            languageSwitcher.element(by.id('toEnglish')).click();
+            expect(element.all(by.id('welcome')).first().getText()).toMatch(/Welcome/);
+        });
+
+        it('should greet user by provided name', function() {
+            element(by.model('homeCtrl.name')).clear();
+            element(by.model('homeCtrl.name')).sendKeys('Héctor');
+
+            var greeting = element(by.binding('homeCtrl.name'));
+
+            expect(greeting.getText()).toEqual('Welcome home, Héctor!');
+            //expect(element.all(by.id('welcome')).first().getText()).toMatch(/Bienvenido/)
+
         });
 
     });
